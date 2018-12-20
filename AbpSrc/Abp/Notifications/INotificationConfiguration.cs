2fbb3e57d338fa171419ea9716b9210db08b07ca
RoyLiu <121407741@qq.com>
2018-12-20 14:26:40 +0800

    include abp source

---
+++ AbpSrc/Abp/Notifications/INotificationConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Abp.Collections;
+
+namespace Abp.Notifications
+{
+    /// <summary>
+    /// Used to configure notification system.
+    /// </summary>
+    public interface INotificationConfiguration
+    {
+        /// <summary>
+        /// Notification providers.
+        /// </summary>
+        ITypeList<NotificationProvider> Providers { get; }
+
+        /// <summary>
+        /// A list of contributors for notification distribution process.
+        /// </summary>
+        ITypeList<INotificationDistributer> Distributers { get; }
+    }
+}
